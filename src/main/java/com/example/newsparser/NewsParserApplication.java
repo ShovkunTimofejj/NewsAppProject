2096b747675d107f32e44a8b1c7df9265eef6355
shovk <shovkyntimofej@gmail.com>
2024-09-30 12:55:10 +0300

    init

---
+++ src/main/java/com/example/newsparser/NewsParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsParserApplication.class, args);
+    }
+
+}
